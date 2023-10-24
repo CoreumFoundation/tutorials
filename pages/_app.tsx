07db45bbeccd53d45305d6cbdcebf85aeeb07130
DDBN <ddbn@gft.com>
2023-10-24 19:12:35 +0200

    Enable keplr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import type { AppProps } from 'next/app';
 
 import { SigningClientProvider } from 'contexts/client';
@@ -10,7 +11,30 @@ import theme from './theme';
 
 import Layout from 'components/Layout';
 
+
+
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(()=>{
+    const chainId = "coreum-testnet-1";
+
+    // Enabling before using the Keplr is recommended.
+    // This method will ask the user whether to allow access if they haven't visited this website.
+    // Also, it will request that the user unlock the wallet if the wallet is locked.
+    //await window.keplr.enable(chainId);
+
+    const offlineSigner = window.keplr.getOfflineSigner(chainId);
+
+    // You can get the address/public keys by `getAccounts` method.
+    // It can return the array of address/public key.
+    // But, currently, Keplr extension manages only one address/public key pair.
+    // XXX: This line is needed to set the sender address for SigningCosmosClient.
+    const accounts = offlineSigner.getAccounts().then(response =>{
+      console.log(response)
+    });
+
+
+
+  },[])
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
