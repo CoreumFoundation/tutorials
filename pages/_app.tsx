dda65bd393d3dbde2f6aafd83bb48798c19dfeb4
Igor Cojocaru <igor.cojocaru@innovamat.com>
2023-10-24 15:04:06 +0200

    update app and document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,25 @@
-import 'styles/globals.css';
 import type { AppProps } from 'next/app';
-import Layout from 'components/Layout';
+
 import { SigningClientProvider } from 'contexts/client';
+
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+
+import '../styles/globals.css';
+import theme from './theme';
+
+import Layout from 'components/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SigningClientProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </SigningClientProvider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <SigningClientProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SigningClientProvider>
+    </ThemeProvider>
   );
 }
 
