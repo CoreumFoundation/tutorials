91a8376b3ebcb77cacdc46f091c1f7066578c022
Yins98 <toni.ds.ds2@gmail.com>
2023-10-25 11:42:44 +0200

    Unity

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,20 +4,26 @@ import CssBaseline from '@mui/material/CssBaseline';
 
 import { SigningClientProvider } from 'contexts/client';
 import { AuthProvider } from 'contexts/AuthContext'; // Import the AuthProvider
+import { useRouter } from 'next/router';
+import {useEffect, useState} from 'react'
 
 import '../styles/globals.css';
 import theme from './theme';
 import Layout from 'components/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const {pathname} = useRouter();
+
+  let isMetaverse = pathname.includes('metaverse')
+
   return (
     <AuthProvider>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <SigningClientProvider>
-          <Layout>
+        <SigningClientProvider>{isMetaverse? <><Component {...pageProps} /></> :<Layout>
             <Component {...pageProps} />
-          </Layout>
+          </Layout>}
+          
         </SigningClientProvider>
       </ThemeProvider>
     </AuthProvider>
--- pages/metaverse/[address].tsx
+++ pages/metaverse/[address].tsx
@@ -1,5 +1,30 @@
+import {useEffect, useState} from 'react'
+import { Unity, useUnityContext } from "react-unity-webgl";
+import { useRouter } from 'next/router';
+
+
 const Metaverse = () => {
-  return <div>Metaverse</div>;
+  const router = useRouter();
+  const [guildAddress, setGuildAddress] = useState<string | null>(null);
+  useEffect(() => {
+    if (!guildAddress) {
+      let address = router.query.address;
+      if (typeof address == 'string') {
+        setGuildAddress(address);
+      }
+    }
+  }, [router]);
+
+  //pages\metaverse\Build\clientBuild.loader.js
+  //console.log(guildAddress);
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "../unity/Build/clientBuild.loader.js",
+    dataUrl: "../unity/Build/clientBuild.data",
+    frameworkUrl: "../unity/Build/clientBuild.framework.js",
+    codeUrl: "../unity/Build/clientBuild.wasm",
+  });
+
+  return <div style={{ width: '100%',display: 'flex', justifyContent: 'center', alignContent: 'center'}}><Unity style={{ minWidth: "92vw", minHeight: "92vh"}} unityProvider={unityProvider} /> </div>;
 };
 
 export default Metaverse;
