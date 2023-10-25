1fc871f1c82ab7ff02effd05d9443514c3b15b96
Igor Cojocaru <igor.cojocaru@innovamat.com>
2023-10-25 22:12:30 +0200

    add tokens display and creation

--- components/Tokens.tsx
+++ components/Tokens.tsx
@@ -1,7 +1,15 @@
 import { useState } from 'react';
 import type { NextPage } from 'next';
 
-import { Box, Button, Tab, Tabs, Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Tab,
+  Tabs,
+  Typography,
+} from '@mui/material';
 
 function CustomTabPanel(props: TabPanelProps) {
   const { children, value, index, ...other } = props;
@@ -30,29 +38,141 @@ function a11yProps(index: number) {
   };
 }
 
-const NoTokensView = () => {
+import {
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from '@mui/material';
+
+function TokenForm() {
+  const [formData, setFormData] = useState({
+    description: '',
+    tokenSymbol: '',
+    type: 'CERC-20',
+    supply: '',
+    decimals: 0,
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-      }}
-    >
-      <Typography variant="body1">
-        You don't have any tokens yet. Create some!
-      </Typography>
-      <Box>
-        <Button variant="contained">Create token</Button>
-      </Box>
+    <Box component="form" sx={{ mt: 3 }}>
+      <TextField
+        margin="normal"
+        label="Description"
+        name="description"
+        value={formData.description}
+        onChange={handleInputChange}
+      />
+      <TextField
+        margin="normal"
+        label="Token Symbol"
+        name="tokenSymbol"
+        value={formData.tokenSymbol}
+        onChange={handleInputChange}
+      />
+      <FormControl margin="normal">
+        <InputLabel>Type</InputLabel>
+        <Select
+          label="Type"
+          name="type"
+          value={formData.type}
+          onChange={handleInputChange}
+          disabled
+        >
+          <MenuItem value="CERC-20">CERC-20</MenuItem>
+        </Select>
+      </FormControl>
+      <TextField
+        margin="normal"
+        label="Supply"
+        name="supply"
+        value={formData.supply}
+        onChange={handleInputChange}
+      />
+      <TextField
+        margin="normal"
+        label="Decimals"
+        name="decimals"
+        type="number"
+        sx={{ width: '8rem' }}
+        value={formData.decimals}
+        onChange={handleInputChange}
+        inputProps={{ min: 0, max: 18 }} // This constrains input to the range 0-18
+      />
+      <Button variant="contained" color="primary" sx={{ mt: 3 }}>
+        Add
+      </Button>
     </Box>
   );
-};
+}
 
-const TokenCreation = () => {
-  let hasTokens = false;
+const MyTokensTab = ({
+  onButtonCreateTokenClick,
+}: {
+  onButtonCreateTokenClick: () => void;
+}) => {
+  let hasTokens = false; // Todo use to display the existing tokens
 
-  return hasTokens ? <p>form</p> : <NoTokensView />;
+  const renderNoTokensView = () => {
+    return (
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          gap: '2rem',
+        }}
+        mt={6}
+      >
+        <Typography variant="body1">
+          You don't have tokens created yet.
+        </Typography>
+        <Box>
+          <Button variant="contained" onClick={onButtonCreateTokenClick}>
+            Create token
+          </Button>
+        </Box>
+      </Box>
+    );
+  };
+
+  const renderTokensView = () => {
+    const tokens = ['token 1', 'token 2']; // TODO: get the tokens from the contract
+
+    return (
+      <Box>
+        <Box
+          sx={{
+            display: 'flex',
+            gap: '2rem',
+          }}
+        >
+          {tokens.map((token) => (
+            <Card key={token}>
+              <CardContent>
+                <Typography variant="h6">Token</Typography>
+                <Typography variant="body1">{token}</Typography>
+              </CardContent>
+            </Card>
+          ))}
+        </Box>
+        <Box mt={4}>
+          <Button variant="contained" onClick={onButtonCreateTokenClick}>
+            Create token
+          </Button>
+        </Box>
+      </Box>
+    );
+  };
+
+  return hasTokens ? renderTokensView() : renderNoTokensView();
 };
 
 const Tokens: NextPage = () => {
@@ -79,10 +199,10 @@ const Tokens: NextPage = () => {
           </Tabs>
         </Box>
         <CustomTabPanel value={tabNumber} index={0}>
-          <TokenCreation />
+          <TokenForm />
         </CustomTabPanel>
         <CustomTabPanel value={tabNumber} index={1}>
-          My tokens
+          <MyTokensTab onButtonCreateTokenClick={() => setTabNumber(0)} />
         </CustomTabPanel>
       </Box>
     </>
