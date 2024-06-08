1203e8c717896f6e2201b7abb2b30688f1405d37
Masih Yeganeh <goodboy.php@gmail.com>
2024-06-08 16:24:08 +0330

    Add asset extension

---
+++ wasm/extension/src/contract.rs
@@ -0,0 +1,95 @@
+#![allow(dead_code, unused_imports)]
+use cosmwasm_std::{entry_point, StdError};
+use cosmwasm_std::{BalanceResponse, BankQuery};
+use cosmwasm_std::{Binary, Coin, Deps, DepsMut, Env, MessageInfo, Response, StdResult, Uint128};
+use cw2::set_contract_version;
+use std::ops::Div;
+
+use crate::error::ContractError;
+use coreum_wasm_sdk::assetft::{
+    self, FrozenBalanceResponse, Query, Token, TokenResponse, WhitelistedBalanceResponse,
+};
+use coreum_wasm_sdk::core::{CoreumMsg, CoreumQueries, CoreumResult};
+/*
+ */
+use crate::msg::{
+    ExecuteMsg, IBCPurpose, InstantiateMsg, MigrateMsg, QueryMsg, SudoMsg, TransferContext,
+};
+use crate::state::DENOM;
+
+// version info for migration info
+const CONTRACT_NAME: &str = env!("CARGO_PKG_NAME");
+const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn instantiate(
+    deps: DepsMut,
+    _env: Env,
+    info: MessageInfo,
+    msg: InstantiateMsg,
+) -> CoreumResult<ContractError> {
+    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+
+    DENOM.save(deps.storage, &msg.denom)?;
+
+    Ok(Response::new()
+        .add_attribute("method", "instantiate")
+        .add_attribute("owner", info.sender))
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn execute(
+    _deps: DepsMut<CoreumQueries>,
+    _env: Env,
+    _info: MessageInfo,
+    msg: ExecuteMsg,
+) -> CoreumResult<ContractError> {
+    match msg {}
+}
+
+#[entry_point]
+pub fn sudo(deps: DepsMut<CoreumQueries>, env: Env, msg: SudoMsg) -> CoreumResult<ContractError> {
+    match msg {
+        SudoMsg::ExtensionTransfer {
+            sender,
+            recipient,
+            transfer_amount,
+            commission_amount,
+            burn_amount,
+            context,
+        } => sudo_extension_transfer(
+            deps,
+            env,
+            transfer_amount,
+            sender,
+            recipient,
+            commission_amount,
+            burn_amount,
+            context,
+        ),
+    }
+}
+
+pub fn sudo_extension_transfer(
+    _deps: DepsMut<CoreumQueries>,
+    _env: Env,
+    _amount: Uint128,
+    _sender: String,
+    _recipient: String,
+    _commission_amount: Uint128,
+    _burn_amount: Uint128,
+    _context: TransferContext,
+) -> CoreumResult<ContractError> {
+    Ok(Response::new())
+}
+
+#[entry_point]
+pub fn migrate(_deps: DepsMut, _env: Env, _msg: MigrateMsg) -> CoreumResult<ContractError> {
+    // No state migrations performed, just returned a Response
+    Ok(Response::default())
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn query(_deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
+    match msg {}
+}
---
+++ wasm/extension/src/msg.rs
@@ -0,0 +1,48 @@
+use cosmwasm_schema::cw_serde;
+use cosmwasm_std::Uint128;
+
+#[cw_serde]
+pub struct InstantiateMsg {
+    pub denom: String,
+    pub user_provided_instantiation_msg: UserProvidedInstantiationMsg,
+}
+
+#[cw_serde]
+pub struct UserProvidedInstantiationMsg {}
+
+#[cw_serde]
+pub enum ExecuteMsg {}
+
+#[cw_serde]
+pub enum SudoMsg {
+    ExtensionTransfer {
+        recipient: String,
+        sender: String,
+        transfer_amount: Uint128,
+        commission_amount: Uint128,
+        burn_amount: Uint128,
+        context: TransferContext,
+    },
+}
+
+#[cw_serde]
+pub struct TransferContext {
+    pub sender_is_smart_contract: bool,
+    pub recipient_is_smart_contract: bool,
+    pub ibc_purpose: IBCPurpose,
+}
+
+#[cw_serde]
+pub enum IBCPurpose {
+    None,
+    Out,
+    In,
+    Ack,
+    Timeout,
+}
+
+#[cw_serde]
+pub struct MigrateMsg {}
+
+#[cw_serde]
+pub enum QueryMsg {}
